4c79bd498890e6513d63ff4625b84dbaf110cd68
HrudhaiUmas <hrudhaiumas20@gmail.com>
2026-01-20 21:16:41 -0800

    finished money struct implementation

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -1,4 +1,5 @@
-struct DomainModel {
+struct DomainModel
+{
     var text = "Hello, World!"
         // Leave this here; this value is also tested in the tests,
         // and serves to make sure that everything is working correctly
@@ -8,14 +9,88 @@ struct DomainModel {
 ////////////////////////////////////
 // Money
 //
-public struct Money {
+public struct Money
+{
+    public let amount : Int;
+    public let currency: String;
+    
+    // Doing what the hint says and will be using a USD based exchange rates
+    private static let rates: [String : Double] =
+    [
+        "USD": 1.0,
+        "GBP": 0.5,
+        "EUR": 1.5,
+        "CAN": 1.25
+    ]
+    
+    public init(amount: Int, currency: String)
+    {
+        if Money.rates[currency] == nil
+        {
+            fatalError("\(currency) is an unknown currency");
+        }
+        else
+        {
+            self.amount = amount;
+            self.currency = currency;
+        }
+    }
+    
+    public func convert(_ targetCurrency: String) -> Money
+    {
+        if Money.rates[self.currency] == nil
+        {
+            fatalError("Unknown source currency: \(self.currency)")
+        }
+
+        // check target currency
+        if Money.rates[targetCurrency] == nil
+        {
+            fatalError("Unknown target currency: \(targetCurrency)")
+        }
+        
+        let sourceCurrencyRate = Money.rates[self.currency]!;
+        let targetCurrencyRate = Money.rates[targetCurrency]!;
+        
+        let amountInUSD = Double(self.amount) / sourceCurrencyRate;
+        let convertedAmount = Int(amountInUSD * targetCurrencyRate);
+        
+        return Money(
+            amount: convertedAmount,
+            currency: targetCurrency
+        )
+    }
+    
+    public func add(_ moneyToAdd: Money) -> Money
+    {
+        let convertedMoneyToAdd = self.convert(moneyToAdd.currency);
+        let totalAmount = moneyToAdd.amount + convertedMoneyToAdd.amount;
+        
+        return Money(
+            amount: totalAmount,
+            currency: moneyToAdd.currency
+        )
+    }
+    
+    public func subtract(_ moneyToSubtract: Money) -> Money
+    {
+        let convertedMoneyToSubtract = self.convert(moneyToSubtract.currency);
+        let resultingAmount = convertedMoneyToSubtract.amount - moneyToSubtract.amount;
+        
+        return Money(
+            amount: resultingAmount,
+            currency: moneyToSubtract.currency
+        )
+    }
 }
 
 ////////////////////////////////////
 // Job
 //
-public class Job {
-    public enum JobType {
+public class Job
+{
+    public enum JobType
+    {
         case Hourly(Double)
         case Salary(UInt)
     }
@@ -24,11 +99,15 @@ public class Job {
 ////////////////////////////////////
 // Person
 //
-public class Person {
+public class Person
+{
+    
 }
 
 ////////////////////////////////////
 // Family
 //
-public class Family {
+public class Family
+{
+    
 }
